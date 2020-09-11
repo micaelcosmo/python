e0045878d3a529a68e975fc96169590334ecbb48
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-11 11:43:40 -0300

    Update ex007.py Sétimo exercício de Python Recebe um nome e duas notas de um aluno, depois te mostra a média entre as notas.

---
+++ PythonExercicios/ex007.py
@@ -0,0 +1,6 @@
+id = input('Para melhor identificação, digite o nome do aluno: ')
+print('Lembre-se de usar ponto lugar da vírgula.')
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1+n2) / 2
+print('A média de {}, é de {}.'.format(id,m))
