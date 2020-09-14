059f5d0b7a496b28b6069f70a2fa5335f8447e46
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-14 14:04:34 -0300

    Update ex014.py Décimo quarto exercício de Python Recebe uma temperatura em Celsius e te mostra a conversão para Fahrenheit.

---
+++ PythonExercicios/ex014.py
@@ -0,0 +1,5 @@
+c = float(input('Digite uma tempera em Celsius: '))
+f = (c * 9 / 5) + 32
+# f = ((9*c)/5)+32
+print('A temperatura de {:.2f}ºC corresponde a {:.2f}ºF.'.format(c,f))
+# de F para C = (f−32)×5/9
