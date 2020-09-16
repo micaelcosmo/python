b41577056db29aea05f0e2ee0258d8acac1b09ff
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-16 16:38:51 -0300

    Update ex016.py Décimo sexto exercício de Python Recebe um número Real qualquer pelo teclado e mostra a sua porção Inteira.

---
+++ PythonExercicios/ex016.py
@@ -0,0 +1,9 @@
+import math
+#   from math import trunc
+r = float(input('Digite um número: '))
+#   #   print('O número {:.3f} tem a parte Inteira {}',.format(r, math.trunc(r)))
+print('O número {:.3f} tem a parte Inteira {}'.format(r, math.floor(r)))
+
+
+#   r = float(input('Digite um número: '))
+#   print('O número {:.3f} tem a parte Inteira {}',.format(r, int(r)))
