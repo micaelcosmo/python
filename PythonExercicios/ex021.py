b6b5553c1fc0327e180d0ed133f06a14a667c95d
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-16 18:08:37 -0300

    Update ex021.py and musicateste.mp3 Vigésimo primeiro exercício de Python Executa um arquivo .mp3 dentro do projeto, utilizando o módulo playsound.

---
+++ PythonExercicios/ex021.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+playsound('musicateste.mp3')
+
+# import pygame
+# pygame.ini()
+# pygame.mixer.music.load('musicateste.mp3')
+# pygame.mixer.music.play()
+# pygame.event.wait()
