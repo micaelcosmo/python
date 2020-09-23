d8468042ff0d04ef67a31783d19e4ae78d89f39c
Micael Cosmo <micael.trabalho@hotmail.com>
2020-09-23 16:46:58 -0300

    Update ex024.py Vigésimo quarto exercício de Python Recebe nome da cidade e verifica se o primeiro nome digitado é identico ao pedido.

---
+++ PythonExercicios/ex024.py
@@ -0,0 +1,8 @@
+nome_cidade = str(input('Digite a cidade que você nasceu: ')).lstrip().lower()
+cc = nome_cidade.startswith('santo')
+print(bool(cc))
+
+'''
+cid = str(input('Nome da cidade '))
+print(cid[:5].upper() == 'SANTO')
+'''
