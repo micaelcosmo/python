4d92d8e93c7cf27de48da1335970d2e587419ca3
Micael Cosmo <micael.trabalho@hotmail.com>
2021-02-23 16:51:01 -0300

    Update ex031.py Trigésimo primeiro exercício de Python Adicionado solução alternativa que é mais enxuta.

--- PythonExercicios/ex031.py
+++ PythonExercicios/ex031.py
@@ -1,4 +1,4 @@
-distancia = int(input('Qual a distancia da viagem em Km? '))
+'''distancia = int(input('Qual a distancia da viagem em Km? '))
 kmPromocao = 200
 taxaCurta = 0.50
 taxaLonga = 0.45
@@ -7,4 +7,10 @@ if distancia <= kmPromocao:
 else:
     taxa = taxaLonga
 resultado = distancia * taxa
-print('O valor da viagem de {}Km é de R${:.2f} reais.'.format(distancia,resultado))
+print('O valor da viagem de {}Km é de R${:.2f} reais.'.format(distancia,resultado))'''
+
+distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a comprar uma viagem de {}Km.'.format(distancia))
+preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print('E o preço da sua passagem será  de RS{:.2f} reais.'.format(preco))
+
