84d6c4c24cfd044fa57d886622655c7c13939103
Micael Cosmo <micael.trabalho@hotmail.com>
2021-02-24 12:20:03 -0300

    Update ex036.py Trigésimo sexto exercício de Python Programa aprovar empréstimo bancário para compra de imóvel, considerando critério para parcelamento.

---
+++ PythonExercicios/ex036.py
@@ -0,0 +1,12 @@
+valorCasa = int(input('Qual o valor do imóvel? '))
+valorSalario = int(input('Qual valor do seu salário? '))
+pagamentoAnos = int(input('Em quantos anos pretende pagar? '))
+
+prestacao = valorCasa / (pagamentoAnos * 12)
+valorMaximo = valorSalario * 0.30
+
+if prestacao <= valorMaximo and pagamentoAnos <= 30:
+    print('{}Parabéns! Seu empréstimo está aprovado.{}'.format('\033[42m', '\033[m'))
+    print('O valor da prestação é de {}R${:.2f}{} reais.'.format('\033[34m', prestacao, '\033[m'))
+else:
+    print('Seu empréstimo foi {}negado{}!'.format('\033[1;41m', '\033[m'))
