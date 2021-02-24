20c9b4298b974d342ea6fed2cf1272e7115094f0
Micael Cosmo <micael.trabalho@hotmail.com>
2021-02-24 10:13:31 -0300

    Update ex032.py Trigésimo segundo exercício de Python Adicionado opção para analizar o ano atual.

--- PythonExercicios/ex032.py
+++ PythonExercicios/ex032.py
@@ -1,11 +1,16 @@
-ano = int(input('Digite o ano: '))
+from datetime import date
+ano = int(input('Que ano quer analizar? Coloque 0 para analizar o ano atual: '))
+if ano == 0:
+    ano = date.today().year
+
 divisivel400 = ano % 400
 divisivel4 = ano % 4
 divisivel100 = ano % 100
+
 if divisivel400 == 0:
-    print('Esse ano é bissexto.')
+    print('Esse ano {} é bissexto.'.format(ano))
 else:
     if divisivel4 == 0 and divisivel100 != 0:
-        print('Esse ano é bissexto.')
+        print('Esse ano {} é bissexto.'.format(ano))
     else:
-        print('Esse ano não é bissexto')
+        print('Esse ano {} não é bissexto'.format(ano))
