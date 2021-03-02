2f305782ccd97e32d5b40859fbead9176f316c00
Micael Cosmo <micael.trabalho@hotmail.com>
2021-03-02 15:29:53 -0300

    Update ex048.py Quadragésimo oitavo exercício de Python Pragrama que soma todos os números ímpares múltiplos de três num intervalo de 1 até 500.

---
+++ PythonExercicios/ex048.py
@@ -0,0 +1,5 @@
+resultado = 0
+for multiplosTres in range(3, 500, 3):
+   if multiplosTres % 2 != 0:
+       resultado += multiplosTres
+print(" O somatório de todos os valores foi {}.".format(resultado))
