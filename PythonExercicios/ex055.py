a32cec6f7789d04d2be2d392a7c62124fdd89bcd
Micael Cosmo <micael.trabalho@hotmail.com>
2021-03-04 15:06:22 -0300

    Update ex055.py Quinquagésimo quinto exercício de Python Lê cinco pesos e mostra qual é maior ou menor.

---
+++ PythonExercicios/ex055.py
@@ -0,0 +1,15 @@
+import sys
+
+QUANTIDADE_PESSOAS = 5
+maior = 0
+menor = sys.maxsize
+
+for p in range(0, QUANTIDADE_PESSOAS):
+    pesos = (float(input("Digite o peso em quilograma(kg): ")))
+    if pesos > maior:
+        maior = pesos
+    if pesos < menor:
+        menor = pesos
+
+print("O maior peso foi {}kg".format(maior))
+print("O menor peso foi {}kg".format(menor))
