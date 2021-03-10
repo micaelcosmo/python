c29b87e22f52afaf14cbf358b9638014dcfa3ba1
Micael Cosmo <micael.trabalho@hotmail.com>
2021-03-10 19:23:48 -0300

    Update ex061.py Sexagésimo primeiro exerícicio Python Lê o primeiro termo e a razão, e mostra os dez primeiros termos em uma progressão aritimética.(while)

---
+++ PythonExercicios/ex061.py
@@ -0,0 +1,7 @@
+inicio = int(input("Digite o primeiro termo: "))
+razao = int(input("Digite a razão: "))
+contador = 0
+while contador < 10:
+    contador += 1
+    print(inicio)
+    inicio += razao
