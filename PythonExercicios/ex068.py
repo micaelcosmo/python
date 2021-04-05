9695fd71c5480ac70dd9f54c8c2356a2212ca5f8
Micael Cosmo <micael.trabalho@hotmail.com>
2021-04-05 12:39:53 -0300

    Update ex068.py Sexagésimo oitavo exercício de Python Jogo do Par ou Ìmpar, mostra as jogada e se o jogador ganhou ou perdeu. O jogo para se for derrota, mostrando quantas venceu.

---
+++ PythonExercicios/ex068.py
@@ -0,0 +1,36 @@
+'''Criar um programa que jogue par ou ímpar e só pare quando o jogador perder'''
+import random
+numero = numero_Computador = contador = total = 0
+resultado = jogador = par_Impar = ''
+while True:
+    escolha_Jogador = str(input('Par ou Ímpar? ->[P/I]<- '))
+    numero = int(input('Digite um valor: '))
+    numero_Computador = random.randint(1, 10)
+    total = numero + numero_Computador
+    if total % 2 == 0:
+        resultado = "P"
+        par_Impar = 'Par'
+        if resultado == escolha_Jogador:
+            contador += 1
+            print(f'Sua escolha foi {escolha_Jogador}')
+            print(f'Você jogou {numero} e o computador {numero_Computador}. Total de {total} deu {par_Impar}.')
+            print('Você GANHOU! Vamos jogar novamente...')
+        elif resultado != escolha_Jogador:
+            print(f'Sua escolha foi {escolha_Jogador}')
+            print(f'Você jogou {numero} e o computador {numero_Computador}. Total de {total} deu {par_Impar}.')
+            print('Você PERDEU!')
+            break
+    else:
+        resultado = "I"
+        par_Impar = 'Ímpar'
+        if resultado == escolha_Jogador:
+            contador += 1
+            print(f'Sua escolha foi {escolha_Jogador}')
+            print(f'Você jogou {numero} e o computador {numero_Computador}. Total de {total} deu {par_Impar}.')
+            print('Você GANHOU! Vamos jogar novamente...')
+        else:
+            print(f'Sua escolha foi {escolha_Jogador}')
+            print(f'Você jogou {numero} e o computador {numero_Computador}. Total de {total} deu {par_Impar}.')
+            print('Você PERDEU!')
+            break
+print(f'GAME OVER!!! Total de vitórias: {contador}')
