1e6536f7f5d3971e87750d891554bcae014c9d3d
Micael Cosmo <micael.trabalho@hotmail.com>
2021-04-17 08:29:58 -0300

    Update ex087.py Octagésimo sétimo exercício de Python Otimizado de o[n²+n²] para o[n²].

--- PythonExercicios/ex087.py
+++ PythonExercicios/ex087.py
@@ -1,5 +1,6 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 pares = soma_Terceira_Coluna = maior_Segunda_Linha = 0
+tabela = ''
 for linha in range(0, 3):
     for coluna in range(0, 3):
         matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
@@ -8,10 +9,9 @@ for linha in range(0, 3):
         soma_Terceira_Coluna += matriz[linha][2]
         if matriz[1][coluna] > maior_Segunda_Linha:
             maior_Segunda_Linha = matriz[1][coluna]
-for linha in range(0, 3):
-    for coluna in range(0, 3):
-        print(f'[{matriz[linha][coluna]:^5}]', end='')
-    print()
+        tabela += f'[{matriz[linha][coluna]:^5}]'
+    tabela += '\n'
+print(tabela)
 print(f'A soma de todos os valores pares é {pares}')
 print(f'A soma dos valores da terceira coluna é {soma_Terceira_Coluna}')
 print(f'O maior valor da segunda coluna é {maior_Segunda_Linha}')
