a7e380f5c34ecf28731b93ae3ad2bc7d50cc6b34
Micael Cosmo <micael.trabalho@hotmail.com>
2021-08-18 21:55:15 -0300

    Update ex092.py Nonagésimo segundo exercício de Python Recebe nome, idade, número de CTPS(se possuir: recebe também ano de contratação e salário) guardando todos os dados em um dicionário. No final, mostra os dados guardados e adiciona "Idade de aposentadoria" caso tenha CTPS.

---
+++ PythonExercicios/ex092.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de Nascimento: '))
+dados['idade'] = datetime.now().year -nasc
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de Contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('*-' * 30)
+for k, v in dados.items():
+    print(f'  - {k} tem o valor {v}')
