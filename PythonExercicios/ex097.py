e7499feac04a74fe245ac65a88d1326ac5bcae83
Micael Cosmo <micael.trabalho@hotmail.com>
2021-09-01 11:30:58 -0300

    Update ex097.py Nonagésimo sétimo exercício de Python Programa com função chamada "escreva()", que recebe qualquer texto como parâmetro e mostra a mensagem com tamanho adaptável.

---
+++ PythonExercicios/ex097.py
@@ -0,0 +1,9 @@
+def escreva(msg):
+    print('~' * (len(msg) + 4))
+    print(f'  {msg}')
+    print('~' * (len(msg) + 4))
+
+
+escreva('Python 3')
+escreva('-> FUNÇÕES <-')
+escreva('Nonagésimo Sétimo Exercício de Python')
