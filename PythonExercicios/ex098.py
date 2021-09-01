fbf071b5611a22277ca49156f894096ac890acdf
Micael Cosmo <micael.trabalho@hotmail.com>
2021-09-01 13:57:23 -0300

    Update ex098.py Nonagésimo oitavo exercício de Python Realocado condições da função contador() para enxutar o código, movido a impressão do cabeçalho para dentro da função tornando-a volátil para a entrada de dados.

--- PythonExercicios/ex098.py
+++ PythonExercicios/ex098.py
@@ -2,29 +2,29 @@ from time import sleep
 
 
 def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    print('~-' * 30)
+    print(f'A contagem de {i} até {f} de {p} em {p}:')
+
     if i <= f:
-        if p <= 0:
-            p = 1
         while i <= f:
             print(f'{i}', end=' ')
             i += p
             sleep(0.3)
     else:
-        if p <= 0:
-            p = 1
         while i >= f:
             print(f'{i}', end=' ')
             i -= p
             sleep(0.3)
     print('Fim!')
-    print('~-' * 30)
 
 
-print('A contagem de 1 até 10 de 1 em 1:')
 contador(1, 10, 1)
-print('A contagem de 10 até 0 de 2 em 2:')
 contador(0, 10, 2)
-
+print('~-' * 30)
 print('Agora é a sua vez de personalizar a contagem.')
 inicio = int(input('Início: '))
 fim = int(input('Fim: '))
