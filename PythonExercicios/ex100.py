c838c785aabac67da1fd1199f8be75096a055a83
Micael Cosmo <micael.trabalho@hotmail.com>
2021-09-14 13:14:23 -0300

    Update ex100.py Centésimo exercício de Python Programa que utiliza duas funções, sorteio() que sorteia 5 números aleatórios de 1 a 10 e soma_par() soma os valores pares. No final, mostra a lista dos números sorteados e o resultado da soma.

---
+++ PythonExercicios/ex100.py
@@ -0,0 +1,24 @@
+from random import randint
+from time import sleep
+
+
+def sorteia(lista):
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('PRONTO!')
+
+
+def soma_par(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+
+numeros = list()
+sorteia(numeros)
+soma_par(numeros)
