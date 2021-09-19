5dcaa996ed75ccf104cc3f206efb3eac9d70a8ba
Micael Cosmo <micael.trabalho@hotmail.com>
2021-09-19 17:13:55 -0300

    Update ex101.py Centésimo primeiro exercício de Python Programa com função chamada voto(), que recebe um ano de nascimento de uma pessoa como parâmetro, e retorna uma mensagem  indicando se a pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.

---
+++ PythonExercicios/ex101.py
@@ -0,0 +1,14 @@
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+
+ano_Nascimento = int(input('Digite o ano de nascimento: '))
+print(voto(ano_Nascimento))
