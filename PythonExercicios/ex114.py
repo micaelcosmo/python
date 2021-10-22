7a357b451325b06c96d943ed7614246a8cb5c8e1
Micael Cosmo <micael.trabalho@hotmail.com>
2021-10-22 12:40:38 -0300

    Update ex114.py Centésimo décimo quarto exercício de Python Faz uma tentativa de acesso ao site "http://www.pudim.com.br" e mostra uma mensagem se está acessível ou não.

---
+++ PythonExercicios/ex114.py
@@ -0,0 +1,9 @@
+import urllib.error
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim com sucesso.')
